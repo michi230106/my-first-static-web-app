4651817153a7e80c178bea72bc927a7827367581
michi230106 <mcr.51378y1d@gmail.com>
2023-08-05 19:21:17 +0900

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello this is first time writting code.</div>;
 }
 
 export default App;
